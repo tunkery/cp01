8a7f7d55a35d164bbd50d14b23e655cb406655e8
Batuhan Olcay <bolcay@2-E-6.42heilbronn.de>
2025-11-04 18:23:19 +0100

    starting the project

---
+++ ex00/Zombie.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bolcay <bolcay@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/04 17:40:06 by bolcay            #+#    #+#             */
+/*   Updated: 2025/11/04 18:04:07 by bolcay           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_H
+# define ZOMBIE_H
+# include <ctime>
+# include <string>
+# include <iostream>
+# include <limits>
+
+class Zombie
+{
+    private:
+        std::string name;
+    
+    public:
+        Zombie( void )
+        {
+        }
+        Zombie( std::string n )
+        {
+            name = n;
+            std::cout << n << ": BraiiiiiiinnnzzzZ..." << std::endl;
+        }
+};
+
+void    randomChump( std::string name);
+Zombie *newZombie( std::string name);
+
+#endif
