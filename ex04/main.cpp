aea83c1afd1f857cabe12fec244d53987c78ef55
Batuhan Olcay <bolcay@1-E-22.42heilbronn.de>
2026-01-20 15:33:50 +0100

    didn't do much but just updating

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bolcay <bolcay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/19 12:23:43 by bolcay            #+#    #+#             */
-/*   Updated: 2026/01/19 15:26:58 by bolcay           ###   ########.fr       */
+/*   Updated: 2026/01/20 13:22:13 by bolcay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,23 @@ std::string	doit(std::string fn, std::string s2, std::string str, std::size_t fo
 	return (0);
 }
 
-int	findit(std::string fn, std::string s1, std::string s2)
+std::size_t	findit(std::string fn, std::string s1, std::string s2)
 {
 	std::size_t found = 1;
 	std::string str;
+	std::size_t size1 = s1.size();
+	std::size_t size2 = s2.size();
 
+	found = fn.find(s1);
+	if (found != 0)
+		str.substr(0, found);
 	while (found != 0)
 	{
 		found = fn.find(s1);
+		str.append()
 		doit(fn, s2, str, found);
 	}
-	return (0);
+	return (found);
 }
 
 int	main(int ac, char **av)
@@ -35,14 +41,17 @@ int	main(int ac, char **av)
 	std::string s1;
 	std::string s2;
 	std::string fn;
+	std::size_t	here;
 	
-	if (ac < 4)
-	{
-		std::cout << "The parameters can't be left empty" << std::endl;
-		return (0);
-	}
-	fn = av[1];
-	s1 = av[2];
-	s2 = av[3];
+	// if (ac < 4)
+	// {
+	// 	std::cout << "The parameters can't be left empty" << std::endl;
+	// 	return (0);
+	// }
+	// fn = av[1];
+	// s1 = av[2];
+	// s2 = av[3];
+	here = findit("abcdefgehjklmeno", "e", "F");
+	std::cout << here << std::endl;
 	return (0);
 }
