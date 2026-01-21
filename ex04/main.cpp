6a98da475910015bf7ee0fc9b01e46acb9a9557a
Batuhan Olcay <bolcay@1-E-30.42heilbronn.de>
2026-01-21 13:22:50 +0100

    the code does replace the things properly kinda but needs improvement.

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bolcay <bolcay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/19 12:23:43 by bolcay            #+#    #+#             */
-/*   Updated: 2026/01/20 13:22:13 by bolcay           ###   ########.fr       */
+/*   Updated: 2026/01/21 13:21:05 by bolcay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,16 +23,30 @@ std::size_t	findit(std::string fn, std::string s1, std::string s2)
 	std::string str;
 	std::size_t size1 = s1.size();
 	std::size_t size2 = s2.size();
+	std::size_t	i;
+	std::size_t	j = 0;
 
 	found = fn.find(s1);
+	std::cout << found << std::endl;
 	if (found != 0)
-		str.substr(0, found);
-	while (found != 0)
 	{
-		found = fn.find(s1);
-		str.append()
-		doit(fn, s2, str, found);
+		str = fn.substr(0, found);
+		str += s2;
 	}
+	std::cout << str << std::endl;
+	i = found;
+	i += size1;
+	while (j != 2)
+	{
+		found = fn.find(s1, i);
+		str.append(fn, i, (found - i));
+		str += s2;
+		i = found;
+		i += size1;
+		j++;
+		// doit(fn, s2, str, found);
+	}
+	std::cout << str << std::endl;
 	return (found);
 }
 
