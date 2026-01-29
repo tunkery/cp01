b0905878e90b6f4e1410e1b82506ec9f28ba4e59
Batuhan Olcay <bolcay@1-E-30.42heilbronn.de>
2026-01-29 12:35:22 +0100

    added the harl messages

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: bolcay <bolcay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/27 15:46:25 by bolcay            #+#    #+#             */
-/*   Updated: 2026/01/27 15:50:55 by bolcay           ###   ########.fr       */
+/*   Updated: 2026/01/29 12:24:39 by bolcay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,20 @@ void	Harl::complain( std::string level)
 
 void	Harl::debug( void )
 {
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
 }
 
 void	Harl::info( void )
 {
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void	Harl::warning( void )
 {
+	std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years, whereas you started working here just last month." << std::endl;
 }
 
 void	Harl::error( void )
 {
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
